03d6beb3d2d227a885c417415c62e5344df8c090
Baris Vural <bariscan.vural@bloomreach.com>
2019-03-09 15:04:09 +0100

    Cleanup code

--- src/main/java/com/github/bcanvural/HstApp.java
+++ src/main/java/com/github/bcanvural/HstApp.java
@@ -1,16 +1,7 @@
 package com.github.bcanvural;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.util.Collections;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
-
-import org.apache.commons.io.IOUtils;
 import org.hippoecm.hst.configuration.model.HstManager;
 import org.hippoecm.hst.configuration.model.HstManagerImpl;
 import org.hippoecm.hst.container.HstDelegateeFilterBean;
@@ -18,7 +9,6 @@ import org.hippoecm.hst.container.HstFilter;
 import org.hippoecm.hst.content.tool.DefaultContentBeansTool;
 import org.hippoecm.hst.mock.core.component.MockHstResponse;
 import org.hippoecm.hst.site.HstServices;
-import org.hippoecm.hst.site.addon.module.model.ModuleDefinition;
 import org.onehippo.cms7.services.ServletContextRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,7 +24,7 @@ public class HstApp {
 
         //Component Manager
         SpringComponentManager componentManager = new SpringComponentManager();
-        componentManager.setAddonModuleDefinitions(Collections.singletonList(loadPageModelAddon()));
+        componentManager.setAddonModuleDefinitions(Collections.singletonList(Utils.loadAddonModule(PAGEMODEL_ADDON_PATH)));
         componentManager.initialize();
         HstServices.setComponentManager(componentManager);
 
@@ -69,39 +59,5 @@ public class HstApp {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    private static ModuleDefinition loadPageModelAddon() {
-        try {
-            URL addonUrl = HstApp.class.getClassLoader().getResource(PAGEMODEL_ADDON_PATH);
-            if (addonUrl == null) {
-                throw new IOException("Error while loading the pagemodel addon module");
-            }
-            return loadModuleDefinition(addonUrl);
-        } catch (IOException | JAXBException e) {
-            LOGGER.error(e.getMessage());
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static ModuleDefinition loadModuleDefinition(URL url) throws JAXBException, IOException {
-        ModuleDefinition moduleDefinition = null;
-
-        JAXBContext jc = JAXBContext.newInstance(ModuleDefinition.class);
-        Unmarshaller um = jc.createUnmarshaller();
-
-        InputStream is = null;
-        BufferedInputStream bis = null;
-
-        try {
-            is = url.openStream();
-            bis = new BufferedInputStream(is);
-            moduleDefinition = (ModuleDefinition) um.unmarshal(url.openStream());
-        } finally {
-            IOUtils.closeQuietly(bis);
-            IOUtils.closeQuietly(is);
-        }
-
-        return moduleDefinition;
     }
 }
