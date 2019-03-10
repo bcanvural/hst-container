5d58abb824c41ac19abf11b07f4e11a05f310a17
Baris Vural <bariscan.vural@bloomreach.com>
2019-03-10 19:57:53 +0100

    Abstract test and a simple unit test

---
+++ src/main/java/com/github/bcanvural/AbstractPageModelTest.java
@@ -0,0 +1,119 @@
+package com.github.bcanvural;
+
+import java.util.Collections;
+
+import javax.annotation.Nullable;
+
+import org.hippoecm.hst.configuration.model.HstManager;
+import org.hippoecm.hst.configuration.model.HstManagerImpl;
+import org.hippoecm.hst.container.HstDelegateeFilterBean;
+import org.hippoecm.hst.container.HstFilter;
+import org.hippoecm.hst.content.tool.DefaultContentBeansTool;
+import org.hippoecm.hst.mock.core.component.MockHstResponse;
+import org.hippoecm.hst.site.HstServices;
+import org.onehippo.cms7.services.ServletContextRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mock.web.MockServletContext;
+
+public abstract class AbstractPageModelTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AbstractPageModelTest.class);
+
+    private SpringComponentManager componentManager;
+    private MockHstRequest hstRequest;
+    private MockHstResponse hstResponse;
+
+    private final static String FILTER_DONE_KEY = "filter.done_" + HstDelegateeFilterBean.class.getName();
+
+    public void init() {
+        setupComponentManager();
+        setupHstRequest();
+        setupServletContext();
+        setupHstResponse();
+    }
+
+    /**
+     * @return Result json as string
+     */
+
+    @Nullable
+    protected String invokeFilter() {
+
+        performValidation();
+        //Invoke
+        HstDelegateeFilterBean filter = componentManager.getComponent(HstFilter.class.getName());
+        try {
+            filter.doFilter(hstRequest, hstResponse, null);
+            String contentAsString = hstResponse.getContentAsString();
+            LOGGER.info(contentAsString);
+            //important! set the filter done attribute to null for subsequent filter invocations
+            hstRequest.setAttribute(FILTER_DONE_KEY, null);
+            return contentAsString;
+        } catch (Exception e) {
+            LOGGER.warn(e.getLocalizedMessage());
+        }
+        return null;
+    }
+
+    private void performValidation() {
+        if (hstRequest.getRequestURI() == null || "".equals(hstRequest.getRequestURI())) {
+            throw new IllegalStateException("Request URI was missing in hstRequest");
+        }
+    }
+
+    protected void setupHstResponse() {
+        this.hstResponse = new MockHstResponse();
+    }
+
+    protected void setupServletContext() {
+        MockServletContext servletContext = new MockServletContext();
+        servletContext.setContextPath("/site");
+        servletContext.setInitParameter(DefaultContentBeansTool.BEANS_ANNOTATED_CLASSES_CONF_PARAM,
+                getAnnotatedHstBeansClasses());
+        hstRequest.setServletContext(servletContext);
+        componentManager.setServletContext(servletContext);
+        ServletContextRegistry.register(servletContext, ServletContextRegistry.WebAppType.HST);
+        HstManagerImpl hstManager = (HstManagerImpl) componentManager.getComponent(HstManager.class);
+        hstManager.setServletContext(hstRequest.getServletContext());
+    }
+
+    protected abstract String getAnnotatedHstBeansClasses();
+
+
+    protected void setupHstRequest() {
+        this.hstRequest = new MockHstRequest();
+//        hstRequest.setRequestURI("/site/resourceapi/news");
+        hstRequest.setContextPath("/site");
+        hstRequest.setHeader("Host", "localhost:8080");
+        hstRequest.setHeader("X-Forwarded-Proto", "http");
+    }
+
+    protected void setupComponentManager() {
+        this.componentManager = new SpringComponentManager();
+        componentManager.setAddonModuleDefinitions(Collections.singletonList(Utils.loadAddonModule(getPageModelAddonPath())));
+        componentManager.initialize();
+        HstServices.setComponentManager(componentManager);
+    }
+
+    /**
+     * Classpath path without the "classpath:". No leading /
+     *
+     * @return
+     */
+    protected abstract String getPageModelAddonPath();
+
+    public SpringComponentManager getComponentManager() {
+        return componentManager;
+    }
+
+    public MockHstRequest getHstRequest() {
+        return hstRequest;
+    }
+
+    public void destroy() {
+
+    }
+
+
+}
