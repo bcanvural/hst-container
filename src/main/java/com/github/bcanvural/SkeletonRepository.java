a55a77df67122ec2046741d3dc22469f6f3f01de
Baris Vural <bariscan.vural@bloomreach.com>
2019-03-15 11:13:56 +0100

    Update dependencies

--- src/main/java/com/github/bcanvural/SkeletonRepository.java
+++ src/main/java/com/github/bcanvural/SkeletonRepository.java
@@ -20,8 +20,8 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
 
-import com.bloomreach.brxm.jcr.repository.InMemoryJcrRepository;
-import com.bloomreach.brxm.jcr.repository.utils.ImporterUtils;
+import com.bloomreach.ps.brxm.jcr.repository.InMemoryJcrRepository;
+import com.bloomreach.ps.brxm.jcr.repository.utils.ImporterUtils;
 
 import static org.hippoecm.repository.api.HippoNodeType.HIPPO_PATHS;
 
